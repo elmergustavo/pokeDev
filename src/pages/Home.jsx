e9931f5a3537ff99e28efb21f09c6cd2504a8ab2
elmergustavo <elmergustavo79@gmail.com>
2022-10-07 19:15:55 -0600

    update text center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
       <Header />
       <section className='mx-auto flex max-w-[1700px] flex-wrap items-center justify-center gap-5 overflow-hidden py-5 transition-colors duration-500 dark:bg-gray-900'>
         <div className='font-Work animate-text bg-gradient-to-r from-rose-300 via-indigo-500 to-teal-100 bg-clip-text text-center font-semibold text-transparent md:text-4xl'>
-          PokeDev desarrollado con React + tailwindcss
+        PokeDev developed with React + tailwindcss
         </div>
       </section>
 
